7c681e1cfda9a1c7f057646e51b4a1406fba0d9d
VitorFernandes00 <68619145+VitorFernandes00@users.noreply.github.com>
2025-03-04 22:40:29 -0300

    ListadeTarefas.js

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
-function receberNumero(valor){
-    if(valor < 6 || valor > 9){
-        console.log("Apenas numeros de 6 até 9");
-        return [] ;
-    }
+let listElement = document.querySelector("#app ul");
+let inputElement = document.querySelector("#app input");
+let buttonElement = document.querySelector("#app button");
 
-    const numeros = [];
+let tarefas = JSON.parse(localStorage.getItem("@listaTarefas")) || [];
 
-    while(numeros.length < valor){
-        const numeroAleatorio = Math.floor(Math.random()* 60) +1;
+function renderTarefas(){
+    listElement.innerHTML = "";
 
-        if(!numeros.includes(numeroAleatorio)){
-            numeros.push(numeroAleatorio)
-        }
-    }
+    tarefas.map((todo)=>{
+        let liElement = document.createElement("li");
+        let tarefaText = document.createTextNode(todo);
 
-    return numeros;
+        let linkElement = document.createElement("a");
+        linkElement.setAttribute("href", "#");
+
+        let linkText = document.createTextNode(" Excluir");
+        linkElement.appendChild(linkText);
+
+        let posicao = tarefas.indexOf(todo);
+
+        linkElement.setAttribute("onclick", `deletarTarefa(${posicao})`);
+
+
+        liElement.appendChild(tarefaText);
+        liElement.appendChild(linkElement);
+        listElement.appendChild(liElement);
+
+    })
 }
 
-const numeroSorteado = receberNumero(6);
-console.log(numeroSorteado);
+renderTarefas();
+
+function adicionarTarefas(){
+    if(inputElement.value === ''){
+        alert("Digite alguma tarefa");
+        return false;
+    }else{
+        let novaTarefa = inputElement.value;
+
+        tarefas.push(novaTarefa);
+        inputElement.value = '';
+
+        renderTarefas();
+        salvarDados();
+    }
+}
+
+buttonElement.onclick = adicionarTarefas;
+
+function deletarTarefa(posicao){
+    tarefas.splice(posicao, 1);
+    renderTarefas();
+    salvarDados();
+}
+
+function salvarDados(){
+    localStorage.setItem("@listaTarefas", JSON.stringify(tarefas));
+}
